c9c89e99e86e0329087f9e1f538208360bf84e83
Hammond Soares <hammond.soares@gmail.com>
2022-03-07 22:53:31 -0500

    Created ProjectController and TicketController

---
+++ TicketerApi/Services/DataServiceFactory.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using TicketerApi.Data;
+
+namespace TicketerApi.Services
+{
+    public class DataServiceFactory
+    {
+        private Dictionary<Type, IDataService> _availableDataServices = new Dictionary<Type, IDataService>();
+
+        public DataServiceFactory(ApplicationDbContext context)
+        {
+            _availableDataServices.Add(typeof(User), new UserDataService(context));
+            _availableDataServices.Add(typeof(Project), new ProjectDataService(context));
+            _availableDataServices.Add(typeof(Ticket), new TicketDataService(context));
+        }
+
+        public IDataService GetDataService(Type type)
+        {
+            return _availableDataServices[type];
+        }
+    }
+}
--- TicketerApi/Services/UserDataService.cs
+++ TicketerApi/Services/UserDataService.cs
@@ -7,56 +7,56 @@ using TicketerApi.Data;
 
 namespace TicketerApi.Services
 {
-    public class DataService
+    public class UserDataService : IGenericDataService<User>
     {
         private ApplicationDbContext _context;
 
-        public DataService(ApplicationDbContext context)
+        public UserDataService(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public async Task<User> AddAsync(User user)
+        public async Task<User> AddAsync(User item)
         {
-            if (user == null)
+            if (item == null)
                 throw new NullReferenceException($"Cannot add null reference object.");
 
-            _context.Users.Add(user);
+            _context.Users.Add(item);
 
             await _context.SaveChangesAsync();
-            return user;
+            return item;
         }
 
-        public async Task DeleteAsync(User user)
+        public async Task DeleteAsync(User item)
         {
-            if (user == null)
+            if (item == null)
                 throw new NullReferenceException($"Cannot delete null reference object.");
 
-            var userToDelete = await GetUserAsync(user.Id);
+            var userToDelete = await GetAsync(item.Id);
 
             _context.Users.Remove(userToDelete);
             await _context.SaveChangesAsync();
         }
 
-        public async Task EditAsync(User user)
+        public async Task EditAsync(User item)
         {
-            if (user == null)
+            if (item == null)
                 throw new NullReferenceException($"Cannot edit null reference object.");
 
-            var dbUser = await GetUserAsync(user.Id);
-            dbUser.Name = user.Name;
-            dbUser.Email = user.Email;
-            dbUser.Password = user.Password;
+            var dbUser = await GetAsync(item.Id);
+            dbUser.Name = item.Name;
+            dbUser.Email = item.Email;
+            dbUser.Password = item.Password;
 
             await _context.SaveChangesAsync();
         }
 
-        public async Task<List<User>> GetUserAsync()
+        public async Task<List<User>> GetAsync()
         {
             return await _context.Users.ToListAsync();
         }
 
-        public async Task<User> GetUserAsync(int id)
+        public async Task<User> GetAsync(int id)
         {
             var user = await _context.Users.FindAsync(id);
             if (user == null)
